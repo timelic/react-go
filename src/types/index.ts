bcb3e72ec7c24a91df79e8c2c3e861b16241ffb7
liujiangfeng <liujiangfeng.1@bytedance.com>
2022-08-28 19:01:43 +0800

    feat: 增加了事件逻辑，增加了生命周期

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,31 @@ export enum PieceState {
 
 export type Board = PieceState[][];
 export type History = Board[];
+
+export type Children = JSX.Element | JSX.Element[] | string | string[];
+
+export interface Player {
+  id: string;
+  name: string;
+}
+
+export interface inviteResponse {
+  isAccepted: boolean;
+  colorAssigned?: PieceState.Black | PieceState.White;
+  // 黑子先行
+}
+
+export enum LifeCycle {
+  NotConnected = "NotConnected", // 未连接
+  Registered = "Registered",
+  Inviting = "Inviting",
+  Playing = "Playing",
+  Considering = "Considering",
+  WaitingToStart = "WaitingToStart",
+}
+
+export enum PlayingLifeCycle {
+  Thinking = "Thinking",
+  WaitingOpponent = "WaitingOpponent",
+  WaitingResp = "WaitingResp",
+}
