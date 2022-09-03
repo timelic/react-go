6610a6f098159debbfd29350c7be385a863d64e3
lf <787587683@qq.com>
2022-09-03 15:10:43 +0800

    feat: 修改吃棋算法，解决死循环以及吃掉自己棋子的问题

--- src/utils/update-table.ts
+++ src/utils/update-table.ts
@@ -47,13 +47,19 @@ const dj = [0, -1, 0, 1]
 function bfs (i: number, j: number, blocks: PieceState[][], isReplace: boolean): boolean {
   // 出界返回true
   if (checkOutOfBounds(i, j, blocks)) return true
+  // 走过的点位记录
+  const dp: Record<string, boolean> = {};
   // 当前点类型
   const p: PieceState = blocks[i][j]
   // 要遍历坐标的数组
   const array:[number, number][] = [[i, j]]
   for (let pos of array) {
-    // 出界按无气算，继续遍历
+    // 出界按无气算，不进行计算
     if (checkOutOfBounds(pos[0], pos[1], blocks)) continue
+    // 该点位走过，不进行计算
+    if (dp[genKey(pos[0], pos[1])]) continue
+    // 标记为已走过
+    dp[genKey(pos[0], pos[1])] = true;
     // 发现空则必定存在气，返回true
     if (blocks[pos[0]][pos[1]] === PieceState.None) return true
     // 如果依然是对方的类型则继续遍历
@@ -103,6 +109,10 @@ export function updateTable(blocks: PieceState[][], i: number, j: number, p: Pie
   let hasLiberty: boolean = true
   // 检查四周
   for (let k = 0; k < 4; k++) {
+    // 出界不检测
+    if (checkOutOfBounds(i + di[k], j + dj[k], blocks)) continue
+    // 只检测是否吃了对方的子，如果探查位置是自己的子则不检测
+    if (blocks[i + di[k]][j + dj[k]] === p) continue
     // 四周的区域任意一个区域无气了则代表存在吃棋
     hasLiberty = bfs(i + di[k], j + dj[k], blocks, true) && hasLiberty
   }
